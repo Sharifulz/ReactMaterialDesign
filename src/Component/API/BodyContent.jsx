845fbee6b0023eeda925fcbd4db7f59607b53589
shaarifulz <shaarifulz@gmail.com>
2020-05-14 23:26:23 +0600

    BodyContent ui for show content get from axios api call

--- src/Component/API/BodyContent.jsx
+++ src/Component/API/BodyContent.jsx
@@ -5,7 +5,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import EmailIcon from '@material-ui/icons/Email';
-import { red, blue, black, yellow } from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
 import PhoneIcon from '@material-ui/icons/Phone';
 import LanguageIcon from '@material-ui/icons/Language';
 
@@ -30,6 +30,18 @@ class BodyContent extends Component{
     render(){
         return(
             <div>
+              <Grid container
+              direction="row"
+              justify="center"
+                    alignItems="center"
+                    spacing={1}
+                    style={{ marginTop:10}}>
+                <Grid iteem sm={3} style={{ marginTop:25}}>Name</Grid>
+                <Grid iteem sm={3} style={{ marginTop:25}}>Email</Grid>
+                <Grid iteem sm={3} style={{ marginTop:25}}>Contacts</Grid>
+                <Grid iteem sm={3} style={{ marginTop:25}}>Website</Grid>
+              </Grid>
+
                 { this.state.persons.map(person =>
                     <Grid container
                     direction="row"
@@ -40,28 +52,28 @@ class BodyContent extends Component{
                         <Grid item sm={3}>
                             <Grid container
                             direction="row">
-                            <Grid item sm={2}><Avatar style={{ backgroundColor: red}}><AccountCircleIcon/></Avatar></Grid>
+                            <Grid item sm={2}><Avatar style={{ backgroundColor: red[500]}}><AccountCircleIcon/></Avatar></Grid>
                             <Grid item sm={10}><Typography noWrap>{person.name}</Typography></Grid> 
                             </Grid>  
                         </Grid>
                         <Grid item sm={3}>
                             <Grid container
                             direction="row">
-                            <Grid item sm={2}><Avatar style={{ backgroundColor: blue}}><EmailIcon/></Avatar></Grid>
+                            <Grid item sm={2}><Avatar style={{ backgroundColor: red[400]}}><EmailIcon/></Avatar></Grid>
                             <Grid item sm={10}><Typography noWrap>{person.email}</Typography></Grid> 
                             </Grid>  
                         </Grid>
                         <Grid item sm={3}>
                             <Grid container
                             direction="row">
-                            <Grid item sm={2}><Avatar style={{ backgroundColor: black}}><PhoneIcon/></Avatar></Grid>
+                            <Grid item sm={2}><Avatar style={{ backgroundColor: red[300]}}><PhoneIcon/></Avatar></Grid>
                             <Grid item sm={10}><Typography noWrap>{person.phone}</Typography></Grid> 
                             </Grid>  
                         </Grid>
                         <Grid item sm={3}>
                             <Grid container
                             direction="row">
-                            <Grid item sm={2}><Avatar style={{ backgroundColor: yellow}}><LanguageIcon/></Avatar></Grid>
+                            <Grid item sm={2}><Avatar style={{ backgroundColor: red[200]}}><LanguageIcon/></Avatar></Grid>
                             <Grid item sm={10}><Typography noWrap>{person.website}</Typography></Grid> 
                             </Grid>  
                         </Grid>
